1c1484e41477294d50389acfba076ef56857496f
OrenBochman <OrenBochman&gmail.com>
2014-08-24 16:04:26 +0300

    added test and fixed some bugs

---
+++ R/Test.R
@@ -0,0 +1,51 @@
+library("testthat")
+
+setwd("~/Documents/R/WRAP")
+#setup
+source("R/WRAP.R")
+#test_file("Test.R")
+
+#testing functions
+expect_that(10, equals(10))
+expect_that(10, is_identical_to(10))
+expect_that(c("one" = 1, "two" = 2),is_equivalent_to(1:2))
+
+#expect_true(x)
+#expect_false(x)
+#expect_is(x, y)
+#expect_equal(x, y)
+#expect_equivalent(x, y)
+#expect_identical(x, y)
+#expect_matches(x, y)
+#expect_output(x, y)
+#expect_message(x, y)
+#expect_warning(x, y)
+#expect_error(x, y)
+
+#create a test
+#res=wmLogin(uri=setApiUri(project="mediawiki"),
+#            "P-value",
+#            "12345678",h,
+#            .opts=c(debugfunction = dbg$update, verbose = TRUE))
+#
+w<-WRAP$new(user="P-value",pass="12345678",email="WRAP@gmail.com")
+
+expect_equal(w$pass, NULL)
+expect_equal(w$user, NULL)
+expect_equal(w$email, NULL)
+
+a <- list(1:10, letters)
+expect_that(str(a), prints_text("List of 2"))
+
+listJSON<-list(
+  logi<-c(TRUE,FALSE,TRUE),
+  num<-c(1.00,3.00,4.15),
+  chr<-c("a","b","def")
+  )
+#str(list4)
+
+expect_equal(w$process_JSON('[ [true, false, true], [1, 3, 4.15], ["a", "b", "def" ] ]')
+             ,listJSON)
+
+testXml<-"<api><edit result=\"Success\" pageid=\"16283969\" title=\"Wikipedia:Sandbox\" contentmodel=\"wikitext\" oldrevid=\"621914847\" newrevid=\"621916124\" newtimestamp=\"2014-08-19T13:57:23Z\"/></api>"
+expect_equal(w$process_XML(doc=testXml,"//api/edit","contentmodel"),"wikitext")
--- R/WRAP.R
+++ R/WRAP.R
@@ -6,82 +6,121 @@ library("RJSONIO") #processing
 #CURRENT: use case - onne wiki available per instance  OR
 #TODO:    use case - multiple wikis available per instance per wiki!
 
-WRAP <- 
-  R6Class("WRAP",
-          private = list(
-            user = NA,
-            pass = NA,
-            e-mail= NA,     
-          ),
-          
-          public = list(        
-            api = NA,
-            version = "1.1",
-            useragent = NA,
-            
-            #constructor
-            initialize = function(user,pass,e-mail,api,curl) 
-            {
-              #TODO load df/table from config.file
-              #if file.exists("wrap.config") initFromFile()
-              
-              #set ctor params
-              if (!missing(api))    self$api <- api
-              else                  self$api <- setApiUri()        
-              if (!missing(user))   self$api <- api        
-              if (!missing(pass))   self$api <- api              
-              if (!missing(e-mail)) self$e-mail <- e-mail
-              if (!missing(curl))   self$curl <- curl
-              else                  self$curl <- getCurlHandle()  
-              
-              #a wmf complient user agent
-              self$useragent = paste("WRAP",self$ver,"contact: ",self$email)
-              
-              #set global curl options
-              curlSetOpt(
-                cookiejar="cookies.txt",    #store cookies, 
-                useragent = self$useragent
-                paste("WRAP ver:",self$ver,
-                      "contact: ",self$email),  #define the user agent
-                followlocation = TRUE,      #continuing queries
-                ssl.verifypeer = FALSE,     #don't verify server cert
-                
-                # proxy=proxy,           #"136.233.91.120",
-                # proxyusername=name,    #"mydomain\\myusername",  
-                # proxypassword=pass,    #'whatever',
-                # proxyport=port         #8080 
-                
-                verbose = TRUE,             #debug 
-                # debugfunction = dfun,          
-                curl=self$curl          #handle is required
-              )
-              
-              
-              
-              #self$getMyLimits()
-              #start up message
-              self$greet()          
-            },
-            
-            setApiUri=function(project="wikipedia",lang="en"){
-              #todo check against legit wm wikis
-              #todo add other wiki (if they support this api)
-              
-              if( nchar(lang)==0){
-                uri=paste("https://www.",project,".org/w/api.php",sep = "")
-              }else{
-                uri=paste("https://",lang,".",project,".org/w/api.php",sep = "")
-              }
-              return(uri)
-            }
-            
-            
-            
-            set_hair = function(val) {
-              self$hair <- val
-            },
-            greet = function() {
-              cat(paste0("Hello, my name is ", self$name, ".\n"))
-            }
-          )
-  )
+WRAP <- R6Class("WRAP",
+                public = list(        
+                  api = NA,
+                  version = "1.0",
+                  useragent = NA,  
+                  curl=NA,
+                  #ctor
+                  initialize = function(user,pass,email,api,curl){
+                    if(file.exists("wrap.config"))
+                      init_from_file("wrap.conf")
+                    else{
+                      #TODO: code me
+                    }              
+                    #set ctor params
+                    if (!missing(api))    self$api=api
+                    else                  self$api=self$set_api_uri()        
+                    if (!missing(user))   private$user=user        
+                    if (!missing(pass))   private$pass=pass              
+                    if (!missing(email)) private$email=email
+                    if (!missing(curl))   private$curl=curl
+                    else                  self$curl=getCurlHandle()  
+                    
+                    #a wmf complient user agent
+                    self$useragent = paste("WRAP",self$ver,"contact: ",self$email)
+                    #set global curl options
+                    curlSetOpt(
+                      cookiejar="cookies.txt",    #store cookies, 
+                      useragent = self$useragent,
+                      followlocation = TRUE,      #continuing queries
+                      ssl.verifypeer = FALSE,     #don't verify server cert
+                      # proxy=proxy,              #"136.233.91.120",
+                      # proxyusername=name,       #"mydomain\\myusername",  
+                      # proxypassword=pass,       #'whatever',
+                      # proxyport=port            #8080 
+                      
+                      verbose = TRUE,             #debug 
+                      # debugfunction = dfun,          
+                      curl=self$curl              #handle is required
+                    )
+                    
+                    #self$getMyLimits()
+                    #start up message
+                    self$greet()          
+                  },
+                  set_api_uri=function(project="wikipedia",lang="en"){
+                    #todo check against legit wm wikis
+                    #todo add other wiki (if they support this api)
+                    
+                    if( nchar(lang)==0){
+                      uri=paste("https://www.",project,".org/w/api.php",sep = "")
+                    }else{
+                      uri=paste("https://",lang,".",project,".org/w/api.php",sep = "")
+                    }
+                    return(uri)
+                  },                      
+                  init_from_file=function(val){},
+                  set_hair = function(val) {
+                    self$hair <- val
+                  },
+                  greet = function() {
+                    cat(paste0("Hello, my name is ", self$name, ".\n"))
+                  },
+                  github_parse = function(req){
+                    text <- content(req, as = "text")
+                    if (identical(text, "")) stop("Not output to parse", call. = FALSE)
+                    jsonlite::fromJSON(text, simplifyVector = FALSE)
+                  },            
+                  process_JSON=function(doc){
+                    result = tryCatch({
+                      fromJSON(I(doc), simplify = TRUE) 
+                    }, warning = function(w) {
+                      #warning-handler-code
+                      #print(paste("warning",w))
+                      
+                    }, error = function(e) {
+                      #error-handler-code
+                      print(paste("warning",e)) 
+                      res=""
+                    }, finally = {
+                      
+                    })        
+                  },#END tryCatch
+                  process_XML=function(doc,xpath,attribute){
+                    result = tryCatch({  
+                      res =xmlInternalTreeParse(doc, trim = TRUE)
+                      res =xpathApply(res,xpath)[[1]]
+                      
+                      if(nchar(attribute)){
+                        res <-xmlAttrs(res)[attribute][[1]]
+                      }
+                      #    res<-xmlAttrs(res)
+                      #    if(attribute %in% res ){#check attrib is in list
+                      #      res <-res[attribute][[1]]
+                      #    }else{
+                      #      e<-simpleError("attribute not found")
+                      #      signalCondition(e)
+                      #    }
+                      #  }
+                      
+                    }, warning = function(w) {
+                      #warning-handler-code
+                      #print(paste("warning",w))
+                    }, error = function(e) {
+                      #error-handler-code
+                      print(paste("warning",e)) 
+                    }, finally = {
+                      
+                    })
+                    return(result)
+                  },#END tryCatch
+                  get_my_limits=function(uri){  max_lag=5 #TODO lookup
+                  }),
+                private = list(
+                  user = NA,
+                  pass = NA,
+                  email= NA,  
+                  max_lag = NA)
+)#class
